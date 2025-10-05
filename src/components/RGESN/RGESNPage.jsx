74e729aa443aa9fd30e8ebabbe23948f7834e2f3
NotSkilix <axelbourdy33@gmail.com>
2025-10-05 14:33:36 +0200

    feat(RGESN): add Analyse component and integrate analysis section into RGESN page

--- src/components/RGESN/RGESNPage.jsx
+++ src/components/RGESN/RGESNPage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../shared/Navbar.jsx";
 import Introduction from "./Introduction.jsx";
 import ButtonTopPage from "../shared/ButtonTopPage.jsx";
 import Audit from "./Audit.jsx";
+import Analyse from "./Analyse.jsx";
 
 const RGESNPage = () => {
     return (
@@ -11,14 +12,28 @@ const RGESNPage = () => {
                 <h1>Application du RGESN</h1>
                 <h2>Audit et amélioration d’un service numérique</h2>
 
+                <div className="mt-4 py-5 px-5 rounded-lg border border-green-200 inline-block">
+                    <h2>Sommaire</h2>
+                    <p>Cette page est séparée en 6 parties:</p>
+                    <ul>
+                        <li><a href={"#introduction"}>Introduction</a></li>
+                        <li><a href={"#audit"}>Audit selon le RGESN</a></li>
+                        <li><a href={"#analyse"}>Analyse et recommendations</a></li>
+                    </ul>
+                </div>
+
                 <div className="mt-8">
                     <div className="mb-4 space-y-4 py-8 px-8 bg-white rounded-lg shadow-md border border-green-200">
-                        <h2>Introduction</h2>
+                        <h2 id={"introduction"}>Introduction</h2>
                         <Introduction />
                     </div>
                     <div className="mb-4 space-y-4 py-8 px-8 bg-white rounded-lg shadow-md border border-green-200">
-                        <h2>Audit</h2>
+                        <h2 id={"audit"}>Audit selon le RGESN</h2>
                         <Audit />
+                    </div>
+                    <div className="mb-4 space-y-4 py-8 px-8 bg-white rounded-lg shadow-md border border-green-200">
+                        <h2 id={"analyse"}>Analyse et recommendations</h2>
+                        <Analyse />
                     </div>
                 </div>
             </div>
